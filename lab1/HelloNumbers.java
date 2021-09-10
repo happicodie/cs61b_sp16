ed970ac31f7c2f77d7a425227a8df615b6a4971c
joseph_glb <josephchow.message@outlook.com>
2021-09-10 22:41:36 +1000

    remove the space while printing

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -2,7 +2,7 @@ public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
         while (x < 10) {
-            System.out.print(x + " ");
+            System.out.print(x);
             x = x + 1;
         }
     }
