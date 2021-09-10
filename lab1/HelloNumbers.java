c6dd0558aedd13c0cee2d7fd5a9831d11e3f0c36
joseph_glb <josephchow.message@outlook.com>
2021-09-10 22:45:53 +1000

    cumulative sum

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,8 +1,10 @@
 public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
+        int cumulative_sum = 0;
         while (x < 10) {
-            System.out.print(x);
+            cumulative_sum = cumulative_sum + x;
+            System.out.print(cumulative_sum + " ");
             x = x + 1;
         }
     }
