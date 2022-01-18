1f7e458ab8c87936384d21ad0d598a18d8727c3e
Joseph Zhou <josephchow.message@gmail.com>
2022-01-19 01:22:38 +1100

    hw0 - DrawTriangle

--- hw0/DrawTriangle.java
+++ hw0/DrawTriangle.java
@@ -1,13 +1,15 @@
 public class DrawTriangle {
     public static void main(String[] args) {
-        int SIZE = 5;
+        int SIZE = Integer.parseInt(args[0]);
         int row, col;
         for (row = 1; row <= SIZE; row++) {
             for (col = 1; col <= row; col++) {
                 System.out.print("*");
             }
-            if (row < 5) {
+            if (row < SIZE) {
                 System.out.print("\n");
+            } else {
+                System.out.println("");
             }
         }
     }
